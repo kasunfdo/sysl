de4340c70ac99281c62df818f898e3498242459e
Kasun <Kasun.Fernando@anz.com>
2019-09-18 11:46:09 +1000

    test win act

--- scripts/test-gosysl.bat
+++ scripts/test-gosysl.bat
@@ -3,7 +3,10 @@ SETLOCAL EnableDelayedExpansion
 
 set ROOT=sysl2\sysl\tests
 for /R %ROOT% %%f in (*.sysl) do (
-	dist\gosysl.exe --log debug pb --mode textpb --root %ROOT% -o %ROOT%\%%~nf.win.txt  /%%~nf.sysl || exit /b !errorlevel!
+	%GOPATH%\bin\sysl.exe --log debug pb --mode textpb --root %ROOT% -o %ROOT%\%%~nf.win.txt  /%%~nf.sysl || exit /b !errorlevel!
 )
 
 del sysl2\sysl\tests\*.win.txt
+
+%GOPATH%\bin\sysl.exe --version
+%GOPATH%\bin\sysl.exe --info
